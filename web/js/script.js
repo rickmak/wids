ea98ed2f4972db3d49bc0992eee577f3ec70a830
Ben Cheng <bencheng@oursky.com>
2012-01-14 20:03:03 +0800

    Add menu navigation

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,18 @@
-/* Author:
+$(function() {
+  $("div#main div").hide();
+  if(window.location.hash) {
+    h = window.location.hash;
+  } else {
+    h = $("nav li.selected a").attr('href');
+  }
+  $('div#main div' + h).show();
 
-*/
-
+  $('nav ul li a').click(function(evt){
+    hash = $(evt.target).attr('href');
+    $("div#main div").hide();
+    $("div#main div" + hash).show();
+  });
+});
 
 
 

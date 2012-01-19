b130fd38fc3613de10bbcabde030e7494e327c36
Ben Cheng <bencheng@oursky.com>
2012-01-19 12:52:12 +0800

    Add menu selected state

--- web/js/script.js
+++ web/js/script.js
@@ -1,16 +1,19 @@
-$(function() {
+navigate = function(hash) {
   $("div#main div").hide();
+  $("div#main div" + hash).show();
+  $('nav ul li').removeClass('selected');
+  $('nav ul li a[href$="'+ hash + '"]').parent().addClass('selected');
+}
+
+$(function() {
   if(window.location.hash) {
     h = window.location.hash;
   } else {
     h = $("nav li.selected a").attr('href');
   }
-  $('div#main div' + h).show();
-
+  navigate(h);
   click_handler = function(evt) {
-    hash = $(evt.currentTarget).attr('href');
-    $("div#main div").hide();
-    $("div#main div" + hash).show();
+    navigate($(evt.currentTarget).attr('href'));
   }
   $('a.nav-btn').click(click_handler);
   $('nav ul li a').click(click_handler);
